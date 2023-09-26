bab7ee66bf01e844a53c6e34e15084ae4956a42e
Matthew Davis <86232146+Mattdavis80@users.noreply.github.com>
2023-09-26 18:59:21 -0230

    Changes to filesystem file

--- task1_filesystem.js
+++ task1_filesystem.js
@@ -8,13 +8,13 @@ const fs = require("fs"); // Require method to import File System module from No
 // First, we will demonstrate the File system module's ability to read text from a file.
 
 // We use the readFileSync method passing both our text file and preferred encoding method and store the value to a variable. If we do not include the encoding argument, it will return a Buffer with Binary data.
-const fileContent = fs.readFileSync("./task1_filesystem.txt", "utf-8");
+const fileContent = fs.readFileSync("./filesystem_text.txt", "utf-8");
 
-// The result is the text contained in the "task1_filesystem.txt file"
+// The result is the text contained in the "filesystem_text.txt file"
 console.log(`This is reading a file synchronously -- ${fileContent}`);
 
 // We can use another method to gather the same information Asynchronously by calling the readFile method as seen below
-fs.readFile("./task1_filesystem.txt", "utf-8", (error, data) => {
+fs.readFile("./filesystem_text.txt", "utf-8", (error, data) => {
   if (error) {
     console.log(error);
   } else {
@@ -24,27 +24,35 @@ fs.readFile("./task1_filesystem.txt", "utf-8", (error, data) => {
 
 // We can also use the File system to write data to a new file. To do this, we can call the writeFile method. By default, the writeFile method will write over any existing text in the file.
 fs.writeFile(
-  "./task1_filesystem_write_example.txt",
-  "Hello new file",
+  "./filesystem_write_example.txt",
+  "This is me writing to a new file!",
   (error) => {
     if (error) {
       console.log(error);
     } else {
-      console.log(`File written --`);
+      const fileContentWrite = fs.readFileSync(
+        "./filesystem_write_example.txt",
+        "utf-8"
+      );
+      console.log(`File written --  Contents: ${fileContentWrite}`);
     }
   }
 );
 
 // To append text to a file, we must set the flag value to "a" for append. We can now see the example file is changed.
 fs.writeFile(
-  "./task1_filesystem_write_example.txt",
+  "./append_example_text.txt",
   " - Adding to the existing file",
   { flag: "a" },
   (error) => {
     if (error) {
       console.log(error);
     } else {
-      console.log(`File appended --`);
+      const fileContentAppend = fs.readFileSync(
+        "./append_example_text.txt",
+        "utf-8"
+      );
+      console.log(`File appended --  Contents: ${fileContentAppend}`);
     }
   }
 );
